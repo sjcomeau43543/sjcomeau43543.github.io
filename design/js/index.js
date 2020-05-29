41572a37aea63029007b1526972afb8f279f5aa6
sj <sjcomeau@wpi.edu>
2020-05-29 11:58:24 -0400

    reorganizing the html and css files for each division

--- design/js/index.js
+++ design/js/index.js
@@ -1,4 +1,51 @@
-// w3
+/*
+Author:        Samantha
+Last modified: 5.5.2020 by sjc
+Status:        In progress
+
+TODO
+1. fix old code
+*/
+
+var production = false;
+/*
+loadFile
+loads files
+*/
+function loadFile(filename, callback) {
+    var xobj = new XMLHttpRequest();
+
+    xobj.overrideMimeType("application/json");
+     // change to ../../ for local https://sjcomeau43543.githu.io/TravelDome/ bfor online
+    if(production) {
+        xobj.open("GET", "https://sjcomeau43543.github.io/TravelDome/"+filename, true);
+    } else {
+        xobj.open("GET", "../../"+filename, true);
+    }
+    xobj.onreadystatechange = function () {
+        if(xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+
+    };
+    xobj.send(null);
+}
+
+/*
+loadPage
+loads a page
+*/
+function loadPage(url){
+    // load content
+    var page = document.getElementById("pageContainerMain");
+    loadFile(url, function(response) {
+        page.innerHTML = response;
+    });
+}
+
+loadPage("home/home.html");
+
+/*/ w3
 
 filterSelection("all")
 function filterSelection(c) {
@@ -46,3 +93,4 @@ for (var i = 0; i < btns.length; i++) {
     console.log(this.className);
   });
 }
+*/
